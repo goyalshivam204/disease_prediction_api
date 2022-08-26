21d674cdc5992a7ed65d14d6ff2e3b3ccd448d7b
Shivam Goyal <shivamaggarwal2042001@gmail.com>
2022-08-26 16:10:30 +0530

    heart model added

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 12 11:36:48 2022
 
-@author: siddhardhan
-"""
-
+from tokenize import Intnumber
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -24,7 +19,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class model_input(BaseModel):
+class diabetes_model_input(BaseModel):
     
     Pregnancies : int
     Glucose : int
@@ -36,12 +31,14 @@ class model_input(BaseModel):
     Age : int
     
 
+
+
 # loading the saved model
 diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
 
 
 @app.post('/diabetes_prediction')
-def diabetes_pred(input_parameters : model_input):
+def diabetes_pred(input_parameters : diabetes_model_input):
     
     input_data = input_parameters.json()
     input_dictionary = json.loads(input_data)
@@ -66,4 +63,64 @@ def diabetes_pred(input_parameters : model_input):
     else:
         return 'The person is Diabetic'
 
+
+
+
+class heart_model_input(BaseModel):
+    
+    age: int
+    sex: int
+    cp: int
+    trestbps: int
+    chol: int
+    fbs: int
+    restecg: int
+    thalach:int
+    exang: int
+    oldpeak: float
+    slope: int
+    ca: int
+    thal: int
+  
+
+
+
+
+
+# loading the saved model
+heart_model = pickle.load(open('heart_model.sav','rb'))
+
+
+@app.post('/heart_prediction')
+def heart_pred(input_parameters : heart_model_input):
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+    
+
+
+
+    age= input_dictionary['age']
+    sex= input_dictionary['sex']
+    cp= input_dictionary['cp']
+    trestbps= input_dictionary['trestbps']
+    chol= input_dictionary['chol']
+    fbs= input_dictionary['fbs']
+    restecg= input_dictionary['restecg']
+    thalach=input_dictionary['thalach']
+    exang= input_dictionary['exang']
+    oldpeak= input_dictionary['oldpeak']
+    slope= input_dictionary['slope']
+    ca= input_dictionary['ca']
+    thal= input_dictionary['thal']
+  
+
+    input_list = [age, sex, cp,trestbps, chol,fbs,restecg,thalach, exang,oldpeak, slope,ca,thal]
+    
+    prediction = heart_model.predict([input_list])
+    
+    if prediction[0] == 0:
+        return 'You have heart problem'
+    
+    else:
+        return 'You have heart problem'
 
